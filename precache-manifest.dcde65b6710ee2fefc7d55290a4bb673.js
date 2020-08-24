1066d04c3ba45707189f973a90ac1b398bc2a6da
yerinny <yerinlee90@gmail.com>
2020-08-24 15:00:52 -0700

    Updates

---
+++ precache-manifest.dcde65b6710ee2fefc7d55290a4bb673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185d60d8412e8fd5024db93a27474cd3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "619f925d7226a225be95",
+    "url": "/monsters-rolodex/static/css/main.4a6c4dfa.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "619f925d7226a225be95",
+    "url": "/monsters-rolodex/static/js/main.77c82db1.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
