b89b3fa58e863b54f1ab0289fbfb6f9d762152e4
yerinny <yerinlee90@gmail.com>
2020-08-24 14:49:58 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
 
@@ -7,8 +11,12 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
+
+    // this.handleChange = this.handleChange.bind(this);
+    // this is not needed anymore because arrow function is used ES6
   }
 
 componentDidMount() {
@@ -18,13 +26,25 @@ componentDidMount() {
 
 }
 
+handleChange = e => {
+  this.setState({ searchField: e.target.value })
+}
+
 
 render() {
+  const { monsters, searchField } = this.state;
+  const filteredMonsters = monsters.filter(monster =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    
   return(
     <div className="App">
-      {
-        this.state.monsters.map(monster => <h1 key={monster.id}> { monster.name } </h1>)
-      }
+    <h1> Monsters Rolodex </h1>
+    <SearchBox
+      placeholder='search monsters' 
+      handleChange={this.handleChange}
+    />
+    <CardList monsters={filteredMonsters} />
     </div>
   )
 }
